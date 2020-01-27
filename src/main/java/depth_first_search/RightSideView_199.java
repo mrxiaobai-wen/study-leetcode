1ac478b9b143007a9e92bd14d0f3ff794bb73ab9
wendiyou <wendiyou@huice.com>
2020-01-27 13:34:30 +0800

    199_树右视

---
+++ src/main/java/depth_first_search/RightSideView_199.java
@@ -0,0 +1,64 @@
+package depth_first_search;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ * <p>
+ * 1            <---
+ * /   \
+ * 2     3         <---
+ * \     \
+ * 5     4       <---
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RightSideView_199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        return fun1(root);
+    }
+
+    /**
+     * 层序遍历
+     *
+     * @param root
+     * @return
+     */
+    private List<Integer> fun1(TreeNode root) {
+        List<Integer> resultList = new ArrayList<>();
+        if (root == null) {
+            return resultList;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == 0) {
+                    resultList.add(node.val);
+                }
+                // 保留走过的节点。对于右边先到底、左边还有节点的情况，通过这一次的循环让其自然淘汰。
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+            }
+        }
+
+        return resultList;
+    }
+}
