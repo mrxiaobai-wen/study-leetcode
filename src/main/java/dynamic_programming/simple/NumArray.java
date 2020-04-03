0b02578ac41b894d55b04d29091fc1c63f1c768f
wendiyou <1129307781@qq.com>
2020-04-03 14:54:25 +0800

    303_区域和检索 - 数组不可变

---
+++ src/main/java/dynamic_programming/simple/NumArray.java
@@ -0,0 +1,43 @@
+package dynamic_programming.simple;
+
+/**
+ * 303. 区域和检索 - 数组不可变
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * <p>
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 说明:
+ * <p>
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ */
+public class NumArray {
+
+    private int[] dp;
+
+    public NumArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = dp[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (dp == null || dp.length == 0) {
+            return 0;
+        }
+        if (i <= 0) {
+            return dp[j];
+        }
+        return dp[j] - dp[i - 1];
+    }
+}
