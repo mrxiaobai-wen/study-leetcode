e1bc9acc26ca280d4eb6090620a75538dd24202a
wendiyou <1129307781@qq.com>
2020-04-19 14:13:24 +0800

    63_不同路径 II

---
+++ src/main/java/dynamic_programming/medium/UniquePathsWithObstacles_63.java
@@ -0,0 +1,70 @@
+package dynamic_programming.medium;
+
+/**
+ * 63. 不同路径 II
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * <p>
+ * <p>
+ * <p>
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ * <p>
+ * 说明：m 和 n 的值均不超过 100。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入:
+ * [
+ * [0,0,0],
+ * [0,1,0],
+ * [0,0,0]
+ * ]
+ * 输出: 2
+ * 解释:
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ */
+public class UniquePathsWithObstacles_63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return fun(obstacleGrid);
+    }
+
+    private int fun(int[][] obs) {
+        int m = obs.length;
+        int n = obs[0].length;
+        int[][] dp = new int[m][n];
+        boolean flag1 = false;
+        for (int i = 0; i < m; i++) {
+            if (obs[i][0] == 1 || flag1) {
+                dp[i][0] = 0;
+                flag1 = true;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        boolean flag2 = false;
+        for (int j = 0; j < n; j++) {
+            if (obs[0][j] == 1 || flag2) {
+                dp[0][j] = 0;
+                flag2 = true;
+            } else {
+                dp[0][j] = 1;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obs[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
