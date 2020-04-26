f9b2e8d9631025b3cf1dbea3b919a141c3437210
wendiyou <1129307781@qq.com>
2020-04-26 22:16:52 +0800

    139_单词拆分

---
+++ src/main/java/dynamic_programming/medium/WordBreak_139.java
@@ -0,0 +1,53 @@
+package dynamic_programming.medium;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 139. 单词拆分
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * <p>
+ * 说明：
+ * <p>
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 示例 2：
+ * <p>
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ * 示例 3：
+ * <p>
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ */
+public class WordBreak_139 {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return fun(s, wordDict);
+    }
+
+    /**
+     * 链接：https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode/
+     */
+    private boolean fun(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length() + 1];
+        Set<String> set = new HashSet<>(wordDict);
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
