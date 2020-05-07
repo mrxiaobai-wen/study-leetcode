ba1f0ce5b97928716e4e391eb52a6cefbe7725c2
wendiyou <1129307781@qq.com>
2020-05-07 21:34:31 +0800

    572_另一个树的子树

---
+++ src/main/java/daily_question/IsSubtree_572.java
@@ -0,0 +1,80 @@
+package daily_question;
+
+import common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 572. 另一个树的子树
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。
+ * s 也可以看做它自身的一棵子树。
+ * <p>
+ * 示例 1:
+ * 给定的树 s:
+ * <p>
+ * 3
+ * / \
+ * 4   5
+ * / \
+ * 1   2
+ * 给定的树 t：
+ * <p>
+ * 4
+ * / \
+ * 1   2
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ * <p>
+ * 示例 2:
+ * 给定的树 s：
+ * <p>
+ * 3
+ * / \
+ * 4   5
+ * / \
+ * 1   2
+ * /
+ * 0
+ * 给定的树 t：
+ * <p>
+ * 4
+ * / \
+ * 1   2
+ * 返回 false。
+ */
+public class IsSubtree_572 {
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return fun(s, t);
+    }
+
+    private boolean fun(TreeNode s, TreeNode t) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(s);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (recursion(cur, t)) {
+                return true;
+            }
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+        return false;
+    }
+
+    private boolean recursion(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return recursion(s.left, t.left) && recursion(s.right, t.right);
+    }
+}
