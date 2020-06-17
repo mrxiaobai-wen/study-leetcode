2150d059e95188513a8dcb9074ceebabb306ebb4
wendiyou <1129307781@qq.com>
2020-06-17 18:13:50 +0800

    343_整数拆分

---
+++ src/main/java/dynamic_programming/medium/IntegerBreak_343.java
@@ -0,0 +1,51 @@
+package dynamic_programming.medium;
+
+import org.junit.Test;
+
+/**
+ * 343. 整数拆分
+ *
+ * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+ *
+ * 示例 1:
+ *
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ * 示例 2:
+ *
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ * 说明: 你可以假设 n 不小于 2 且不大于 58。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IntegerBreak_343 {
+
+    @Test
+    public void test1() {
+        System.out.println(fun(10));
+    }
+
+    public int integerBreak(int n) {
+
+        return fun(n);
+    }
+
+    private int fun(int n) {
+        int[] dp = new int[59];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3;i <= n;i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = 1;j < i;j++) {
+                max = Math.max(Math.max(dp[i - j],i - j) * Math.max(dp[j],j),max);
+            }
+            dp[i] = max;
+        }
+        return dp[n];
+    }
+}
