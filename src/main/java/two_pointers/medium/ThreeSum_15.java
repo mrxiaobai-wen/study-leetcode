96e68ec582d809ed099b57033042edd90ae223a3
wendiyou <1129307781@qq.com>
2020-10-09 13:44:25 +0800

    15_三数之和

---
+++ src/main/java/two_pointers/medium/ThreeSum_15.java
@@ -0,0 +1,106 @@
+package two_pointers.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 15. 三数之和
+ *
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 示例：
+ *
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ *
+ * 满足要求的三元组集合为：
+ * [
+ *   [-1, 0, 1],
+ *   [-1, -1, 2]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ThreeSum_15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        //return fun(nums);
+        return fun2(nums);
+    }
+
+    /**
+     * 双指针
+     */
+    private List<List<Integer>> fun2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length <= 2) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0;i < nums.length - 2;i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    result.add(new ArrayList<>(Arrays.asList(nums[i],nums[left],nums[right])));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 三重循环
+     * </p>
+     * 超时
+     */
+    private List<List<Integer>> fun(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0;i < nums.length - 2;i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1;j < nums.length - 1;j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                for (int k = j + 1;k < nums.length;k++) {
+                    if (k != j + 1 && nums[k] == nums[k - 1]) {
+                        continue;
+                    }
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        result.add(list);
+                        break;
+                    } else if (nums[i] + nums[j] + nums[k] > 0){
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
