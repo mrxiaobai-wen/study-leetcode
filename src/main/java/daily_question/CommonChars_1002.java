464418a96b204d9e8624878ba7cc9afa15c8fbd5
wendiyou <1129307781@qq.com>
2020-10-14 14:49:48 +0800

    1002_查找常用字符

---
+++ src/main/java/daily_question/CommonChars_1002.java
@@ -0,0 +1,60 @@
+package daily_question;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1002. 查找常用字符
+ *
+ * 给定仅有小写字母组成的字符串数组 A，返回列表中的每个字符串中都显示的全部字符（包括重复字符）组成的列表。例如，如果一个字符在每个字符串中出现 3 次，
+ * 但不是 4 次，则需要在最终答案中包含该字符 3 次。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ * 示例 1：
+ *
+ * 输入：["bella","label","roller"]
+ * 输出：["e","l","l"]
+ * 示例 2：
+ *
+ * 输入：["cool","lock","cook"]
+ * 输出：["c","o"]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= A.length <= 100
+ * 1 <= A[i].length <= 100
+ * A[i][j] 是小写字母
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-common-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CommonChars_1002 {
+
+    @Test
+    public void test() {
+        List<String> list1 = commonChars(new String[]{"bella","label","roller"});
+        System.out.println(list1);
+        List<String> list2 = commonChars(new String[]{"cool","lock","cook"});
+        System.out.println(list2);
+    }
+
+    public List<String> commonChars(String[] A) {
+        int[] arr = new int[26];
+        List<String> list = new ArrayList<>();
+        for (String str : A) {
+            for (char tempChar : str.toCharArray()) {
+                arr[tempChar - 'a'] += 1;
+                if (arr[tempChar - 'a'] == 3) {
+                    list.add(tempChar + "");
+                    arr[tempChar - 'a'] = 0;
+                }
+            }
+        }
+        return list;
+    }
+}
