e8f13c6de46fcb63ea7cbd355cdb88d035345b27
wendiyou <1129307781@qq.com>
2020-10-18 12:17:56 +0800

    287_寻找重复数

---
+++ src/main/java/two_pointers/medium/FindDuplicate_287.java
@@ -0,0 +1,56 @@
+package two_pointers.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 287. 寻找重复数
+ *
+ * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+ * 假设只有一个重复的整数，找出这个重复的数。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,4,2,2]
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [3,1,3,4,2]
+ * 输出: 3
+ * 说明：
+ *
+ * 不能更改原数组（假设数组是只读的）。
+ * 只能使用额外的 O(1) 的空间。
+ * 时间复杂度小于 O(n2) 。
+ * 数组中只有一个重复的数字，但它可能不止重复出现一次。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-the-duplicate-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindDuplicate_287 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(2,findDuplicate(new int[]{1,3,4,2,2}));
+        Assert.assertEquals(3,findDuplicate(new int[]{3,1,3,4,2}));
+    }
+
+    public int findDuplicate(int[] nums) {
+        return fun(nums);
+
+        // todo 官方题解 二进制、快慢指针
+        // todo https://leetcode-cn.com/problems/find-the-duplicate-number/solution/xun-zhao-zhong-fu-shu-by-leetcode-solution/
+    }
+
+    private int fun(int[] nums) {
+        for (int i = 0;i < nums.length - 1;i++) {
+            for (int j = i + 1;j < nums.length;j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return 0;
+    }
+}
