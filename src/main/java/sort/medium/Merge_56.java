ba19390129ec05ada2323dfc920950c55f829980
wendiyou <1129307781@qq.com>
2021-03-08 11:37:40 +0800

    56. 合并区间

---
+++ src/main/java/sort/medium/Merge_56.java
@@ -0,0 +1,70 @@
+package sort.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ *
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+ * 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ *
+ * 示例 1：
+ *
+ * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+ * 输出：[[1,6],[8,10],[15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * 示例 2：
+ *
+ * 输入：intervals = [[1,4],[4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ *
+ * 提示：
+ *
+ * 1 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 104
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-intervals
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Merge_56 {
+
+    public int[][] merge(int[][] intervals) {
+        return fun(intervals);
+    }
+
+    public int[][] fun(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0;i < intervals.length;i++) {
+            int[] cur = intervals[i];
+            if (list.isEmpty()) {
+                list.add(cur);
+            } else {
+                int[] last = list.get(list.size() - 1);
+                if (cur[0] > last[1]) {
+                    list.add(cur);
+                } else {
+                    last[1] = Math.max(last[1],cur[1]);
+                }
+            }
+        }
+        int[][] result = new int[list.size()][2];
+        list.toArray(result);
+        return result;
+    }
+}
