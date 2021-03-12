fec156b3e04f55e5331e13f4341c7b009427e5cb
wendiyou <1129307781@qq.com>
2021-03-12 16:35:14 +0800

    136. 只出现一次的数字

---
+++ src/main/java/bit_manipulation/easy/SingleNumber_136.java
@@ -0,0 +1,38 @@
+package bit_manipulation.easy;
+
+/**
+ * 136. 只出现一次的数字
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ * 示例 2:
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SingleNumber_136 {
+
+    public int singleNumber(int[] nums) {
+        return fun(nums);
+    }
+
+    /**
+     * 使用位运算，关键在于：位运算满足交换律和结合律!!!
+     */
+    private int fun(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
